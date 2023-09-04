2c6d029cb298cae6bccca0e70087342d2483a03a
lolocro <bosniclovro@gmail.com>
2023-09-04 21:01:34 +0200

    tested fastify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RouteConfig } from '@nestjs/platform-fastify';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @RouteConfig({ output: 'hello world' })
+  @Get('/hello')
+  index(@Req() req) {
+    return req.routeConfig.output;
   }
 }
